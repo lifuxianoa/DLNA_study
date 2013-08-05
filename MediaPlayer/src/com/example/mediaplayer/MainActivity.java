d851037d8e4352082d41cf1379ae5c9a056a3ac5
lifuxian <lifuxianoa@163.com>
2013-08-05 17:49:53 +0800

    添加设备列表更新功能。

--- MediaPlayer/src/com/example/mediaplayer/MainActivity.java
+++ MediaPlayer/src/com/example/mediaplayer/MainActivity.java
@@ -1,40 +1,81 @@
 package com.example.mediaplayer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.cybergarage.upnp.Device;
+import org.cybergarage.upnp.DeviceList;
+import org.cybergarage.upnp.device.DeviceChangeListener;
 import org.cybergarage.upnp.std.av.controller.MediaController;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener,
+	OnItemClickListener, DeviceChangeListener{
 
-	private final static String TAG = "MediaPlayer";
-	@SuppressLint("NewApi")
+	private final static String TAG = "MyMediaPlayer";
+	private final static int REFRESH_LIST = 1;
+	
+	private Button mBtnSearch;
+	private Button mBtnReset;
+	private Button mBtnExit;
+	private ListView mListDev;
+	private MediaController mMediaController;
+	private ArrayList<HashMap<String, Object>> mListItem;
+	private SimpleAdapter mListItemAdapter;
+	private Handler mHandler;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mBtnSearch = (Button)findViewById(R.id.btn_search);
+		mBtnReset = (Button)findViewById(R.id.btn_reset);
+		mBtnExit = (Button)findViewById(R.id.btn_exit);
+		mListDev = (ListView)findViewById(R.id.list_device);
 		
-		if (android.os.Build.VERSION.SDK_INT > 9) {
-		    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-		    StrictMode.setThreadPolicy(policy);
-		}
+		mBtnSearch.setOnClickListener(this);
+		mBtnReset.setOnClickListener(this);
+		mBtnExit.setOnClickListener(this);
+		mListDev.setOnItemClickListener(this);
 		
-		MediaController mController = new MediaController();
-		mController.start();
-		Log.e(TAG, "after start(), sleep...");
-		try {
-			Thread.sleep(1000 * 5);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		Log.e(TAG, "before print(), wakeup...");
-		mController.printMediaServers();
-		mController.stop();
+		mMediaController = new MediaController();
+		mMediaController.addDeviceChangeListener(this);
+		
+		mListItem = new ArrayList<HashMap<String,Object>>();
+		mListItemAdapter = new SimpleAdapter(this, mListItem, 
+				R.layout.device_list, 
+				new String[] {"name", "location", "devicetype"},
+				new int[] {R.id.name, R.id.location, R.id.devicetype});
+		mListDev.setAdapter(mListItemAdapter);
+		
+		mHandler = new Handler(){
+
+			@Override
+			public void handleMessage(Message msg) {
+				switch (msg.what) {
+				case REFRESH_LIST:
+					mListItemAdapter.notifyDataSetChanged();
+					break;
+
+				default:
+					break;
+				}
+			}
+		};
 	}
 
 	@Override
@@ -42,6 +83,75 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_search:
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					mMediaController.start();
+				}
+			}).start();
+			break;
+			
+		case R.id.btn_reset:
+			mMediaController.stop();
+			break;
+			
+		case R.id.btn_exit:
+			mMediaController.stop();
+			finish();
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void refreshDeviceList()
+	{
+		DeviceList devList = mMediaController.getDeviceList();
+		mListItem.clear();
+		for (int i = 0; i < devList.size(); i++) {
+			Device device = devList.getDevice(i);
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("name", device.getFriendlyName());
+			map.put("location", device.getLocation());
+			map.put("devicetype", device.getDeviceType());
+			mListItem.add(map);
+		}
+		
+		Message msg = new Message();
+		msg.what = REFRESH_LIST;
+		mHandler.sendMessage(msg);
+	}
+	
+	@Override
+	public void deviceAdded(Device dev) {
+		if (null != dev) {
+			Log.d(TAG, "deviceAdded: " + dev.getFriendlyName());
+			refreshDeviceList();
+		}
+	}
+
+	@Override
+	public void deviceRemoved(Device dev) {
+		if (null != dev) {
+			Log.d(TAG, "deviceRemoved: " + dev.getFriendlyName());
+			refreshDeviceList();
+		}
 	}
 
 }
